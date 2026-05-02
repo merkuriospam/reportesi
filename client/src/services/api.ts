ca8cc5f23032f3cc6cdc93323bf763e825a84d4d
merkuriospam <merkuriospam@gmail.com>
2026-05-02 12:18:31 -0300

    api prod

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  // baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://reportesi/api',
 });
 
 api.interceptors.request.use((config) => {
