ff00fb14d9a9e87a37d9fc5e31ab4fa871de937c
merkuriospam <merkuriospam@gmail.com>
2026-07-28 17:34:12 -0300

    migratePlopiAr

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3001/api',
-  baseURL: 'https://reportesi.plopi.com.ar/api',
+  // baseURL: 'https://reportesi.plopi.com.ar/api',
+  baseURL: 'https://reportesi.plopi.ar/api',
 });
 
 api.interceptors.request.use((config) => {
